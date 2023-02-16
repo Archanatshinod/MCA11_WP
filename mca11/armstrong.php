54700b806f216330ad11b6d537b97dfe3ed7da4d
Archanatshinod <116487133+Archanatshinod@users.noreply.github.com>
2023-02-16 01:22:05 -0800

    Add files via upload

---
+++ mca11/armstrong.php
@@ -0,0 +1,21 @@
+<?php
+function isArmstrong($number){
+$sum=0;
+$temp=$number;
+$num_digits=strlen($number);
+while($temp!=0)
+{
+$digit=$temp%10;
+$sum+=pow($digit,$num_digits);
+$temp/=10;
+}
+return $sum==$number;
+}
+$num=1634;
+if(isArmstrong($num)){
+echo "$num is an armstrong number";
+}else
+{
+echo "$num is not armstrong";
+}
+?>
